2603c8fe9e559817b70b54673e86ec6b620dc448
欧阳斌 <ouyangbin81076@gmail.com>
2018-12-06 18:26:40 +0800

    add download test

--- src/download.ts
+++ src/download.ts
@@ -1,7 +1,5 @@
-import { existsSync, mkdirSync, writeFileSync } from "fs";
-import { join } from "path";
 import { loadCities, loadCounties, loadProvinces, loadTowns, loadVillages } from "./loader";
-interface IEntity {
+export interface IEntity {
   code: number;
   name: string;
   children?: IEntity[];
@@ -10,24 +8,16 @@ const loaders: Array<(url: string) => Promise<Array<{ code: number, name: string
   loadProvinces, loadCities, loadCounties, loadTowns, loadVillages,
 ];
 
-export async function start({ depth, url }: { depth: number, url: string }) {
-  const data = await load(url, 0, depth);
-  console.log("加载完毕");
-  if (!existsSync(join(__dirname, "../dist"))) {
-    mkdirSync(join(__dirname, "../dist"));
+export async function download(url: string, index: number, depth: number, log = true): Promise<IEntity[]> {
+  if (log) {
+    console.log(`%s %s %s`, index, depth, url);
   }
-  const dest = join(__dirname, "../dist/data.json");
-  writeFileSync(dest, JSON.stringify(data, null, 2));
-  console.log("文件已经成功写入到 " + dest);
-}
-async function load(url: string, index: number, depth: number): Promise<IEntity[]> {
-  console.log(`%s %s %s`, index, depth, url);
   const data = await loaders[index](url);
   if (index < depth - 1) {
     const arr: IEntity[] = [];
     for (const { code, name, link } of data) {
       if (link) {
-        arr.push({ code, name, children: await load(link, index + 1, depth) });
+        arr.push({ code, name, children: await download(link, index + 1, depth) });
       } else {
         arr.push({ code, name });
       }
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
 // tslint:disable:no-console
 import commander from "commander";
-import { start } from "./download";
+import { existsSync, mkdirSync, writeFileSync } from "fs";
+import { join } from "path";
+import { download } from "./download";
 const program = commander
   .description("统计局爬虫")
   .option("-d, --depth [depth]", "搜索深度1:省,2:市,3:区,4:镇,5:办事处", "1")
   .option("-u, --url [url]", "网址", "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2017/index.html")
   .parse(process.argv);
-start({
-  depth: parseInt(program.depth, 10),
-  url: program.url,
-}).catch((e) => console.error(e));
+const main = async () => {
+  const depth = parseInt(program.depth, 10);
+  const url = program.url;
+  const index = 0;
+  const data = await download(url, index, depth);
+  console.log("加载完毕");
+  if (!existsSync(join(__dirname, "../dist"))) {
+    mkdirSync(join(__dirname, "../dist"));
+  }
+  const dest = join(__dirname, "../dist/data.json");
+  writeFileSync(dest, JSON.stringify(data, null, 2));
+  console.log("文件已经成功写入到 " + dest);
+};
+main().catch((e) => console.error(e));
